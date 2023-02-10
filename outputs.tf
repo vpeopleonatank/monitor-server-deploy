bb0b959eec6b28074773519a08000632191ada38
vpeopleonatank <vpeopleonatank@gmail.com>
2023-02-10 10:57:24 +0700

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ip" {
-  value = libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]
+  value = libvirt_domain.domain-zabbix.network_interface[0].addresses[0]
 }
 
 output "url" {
-  value = "http://${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}"
+  value = "http://${libvirt_domain.domain-zabbix.network_interface[0].addresses[0]}"
 }
